1a52c1e56e9b4724fb248c26ed26eab8b255836c
seva <lopachuk.seva@gmail.com>
2023-06-21 14:35:36 +0200

    added admin panel settings for products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from products.models import ProductCategory, Product
 
-# Register your models here.
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
