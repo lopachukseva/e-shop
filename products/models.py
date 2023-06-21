fddae637b4b5c147a476a970eab2ddeae8cc63f4
seva <lopachuk.seva@gmail.com>
2023-06-21 14:36:10 +0200

    added str bound method to models

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
