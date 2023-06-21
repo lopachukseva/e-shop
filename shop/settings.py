e4bfa4117d77c665695da63c5cbe54d694c94b32
seva <lopachuk.seva@gmail.com>
2023-06-22 00:32:31 +0200

    changed language from en to ru

--- shop/settings.py
+++ shop/settings.py
@@ -94,7 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
 TIME_ZONE = "UTC"
 
@@ -118,3 +119,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
