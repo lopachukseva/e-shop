8bad0144c38a96421ad26a8210af991326f1cd48
seva <lopachuk.seva@gmail.com>
2023-06-21 14:37:08 +0200

    added products and categories from db

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request):
@@ -11,25 +12,7 @@ def index(request):
 def products(request):
     context = {
         "title": "Catalog",
-        "products": [
-            {
-                "name": "Худи черного цвета с монограммами adidas Originals",
-                "description": "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
-                "image": "/static/vendor/img/products/Adidas-hoodie.png",
-                "price": 4500
-            },
-            {
-                "name": "Синяя куртка The North Face",
-                "description": "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.",
-                "image": "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
-                "price": 3900
-            },
-            {
-                "name": "Коричневый спортивный oversized-топ ASOS DESIGN",
-                "description": "Материал с плюшевой текстурой. Удобный и мягкий.",
-                "image": "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
-                "price": 7800
-            },
-        ]
+        "products": Product.objects.all(),
+        "categories": ProductCategory.objects.all(),
     }
     return render(request, "products/products.html", context)
