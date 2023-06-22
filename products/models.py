af0e2915e3af07d74e99ab9150813b9798baf75e
seva <lopachuk.seva@gmail.com>
2023-06-23 00:21:57 +0200

    added basket model added basket custom manager

--- products/models.py
+++ products/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+from users.models import User
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
@@ -17,5 +23,32 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
+
     def __str__(self):
         return self.name
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(item.sum() for item in self)
+
+    def total_quantity(self):
+        return sum(item.quantity for item in self)
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self):
+        return f"Корзина для {self.user.username}, Категория: {self.product.name}"
+
+    def sum(self):
+        return self.product.price * self.quantity
