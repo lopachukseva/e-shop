10e22a697539f635912bb3aca28495fce089b8ac
seva <lopachuk.seva@gmail.com>
2023-06-23 00:20:20 +0200

    add settings to admin

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
 
+@admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "price", "quantity", "category")
+    fields = ("name", "description", ("price", "quantity"), "image", "category")
+    search_fields = ("name",)
+    ordering = ("name",)
 
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ("product", "quantity")
+    extra = 0
--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdmin
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(User)
+    list_display = ("username", )
+    inlines = BasketAdmin,
