80ad63b4eb14c29571e91a8aff3ba348054a07f2
seva <lopachuk.seva@gmail.com>
2023-06-23 00:22:26 +0200

    added urls for categories and pagination

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,9 @@ from products import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("products/", views.products, name="products")
+    path("products/", views.products, name="products"),
+    path("products/category/<int:category_id>", views.products, name="category"),
+    path("products/page/<int:page_number>", views.products, name="paginator"),
+    path("basket/add/<int:product_id>", views.basket_add, name="basket_add"),
+    path("basket/remove/<int:basket_id>", views.basket_remove, name="basket_remove"),
 ]
