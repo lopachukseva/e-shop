3008aab570bf054fca56448cfc121e6e8cdfc381
seva <lopachuk.seva@gmail.com>
2023-06-28 16:29:32 +0200

    added redis cache

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from products import views
 
+# from django.views.decorators.cache import cache_page
+
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("products/", views.ProductsListView.as_view(), name="products"),
+    # path("products/", cache_page(30)(views.ProductsListView.as_view()), name="products"),
     path("products/category/<int:category_id>", views.ProductsListView.as_view(), name="category"),
     path("products/page/<int:page>", views.ProductsListView.as_view(), name="paginator"),
     path("basket/add/<int:product_id>", views.basket_add, name="basket_add"),
